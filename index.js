0ed0863a6c63a3c3f6bc4b1712c372e420edc1a2
Nicole Young <coley504@gmail.com>
2018-08-12 20:44:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const  currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting= `Welcome, ${currentUser}`;
-currentUser.slice(0, 1)
+currentUser.slice(0, 1);
